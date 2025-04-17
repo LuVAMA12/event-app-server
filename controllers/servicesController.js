98204c5ab1e7c0659b2920f20797f55110e7a20f
LuVAMA12 <jacqueraylucie@gmail.com>
2025-04-17 10:46:42 +0200

    add populate function on the get of all services

--- controllers/servicesController.js
+++ controllers/servicesController.js
@@ -2,7 +2,7 @@ import Service from "../models/Service.js"
 
 export const getAllServices = async (req, res) => {
     try {
-        const services = await Service.find()
+        const services = await Service.find().populate('userID', '-password')
         if(services.length < 1) {
             return res.status(404).json('Services not found')
         }
